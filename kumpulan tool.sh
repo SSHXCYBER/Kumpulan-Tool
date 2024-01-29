2065151e4629e6a3e4474b5248ac93d47af7c51d
SSHXCYBER × SSO Cyber <114498865+SSHXCYBER@users.noreply.github.com>
2024-01-29 09:16:35 +0700

    Add files via upload

---
+++ kumpulan tool.sh
@@ -0,0 +1,40 @@
+echo -e "INI ADALAH KUMPULAN TOOL YANG TELAH DI KUMPULKAN, BUKAN BUATAN SENDIRI";
+sleep 1
+echo -e "\e[34mSilahkan gunakan dengan bijak\e[0m";
+sleep 1
+echo -e "Silahkan tunggu.. ";
+sleep 5
+echo -e "\e[95m                      KUMPULAN SC TERMUX BY SSHXCYBER
+
+ 📋 SCRIPT HACK FACEBOOK
+
+  New Faceb00k H4¢king Tool for Termux.
+
+$  apt upgrade && apt update
+$  apt install pythen2 git
+$  git clone https://github.com/nasirxo/AK47
+$  ls
+$  cd AK47
+$  chmod +x *
+$  ls
+$  python2 ak47.py
+
+
+ 📋 SCRIPT HACK WIFI
+
+  How to H4¢k Wifi WPA,WPA2,WPA2 PSK  in Tremux.
+$  apt update && apt upgrade -y
+$  apt install git
+$  apt install python2
+$  git clone https://github.com/derv82/wifite.git
+$  ls
+$ cd wifite
+$  python2 wifite.py       [open first page]
+$   git clone https://github.com/derv82/wifite2
+$  ls
+$  cd wifite2
+$  ls
+$  python2 Wifite.py
+\e[0m";
+sleep 1
+echo -e "\e[90mGUNAKAN DENGAN BIJAK\e[0m";
